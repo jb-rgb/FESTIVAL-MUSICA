89bc62332cd2838f3782b243268398528d585879
Jorge Barahona <bacj010509@gs.utm.mx>
2023-12-27 12:17:21 -0600

    Primer commit.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+function css (done) {
+    src('src/scss/**/*.scss') // Identificar el archivo SASS
+        .pipe( plumber() )
+        .pipe( sass() ) // Compilarlo
+        .pipe( dest("build/css") ); // Almacenarlo en disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final de la tarea
+}
+
+function dev (done) {
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
