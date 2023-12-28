abd778f3335686676cf0927554b32bb05c46a383
Jorge Barahona <bacj010509@gs.utm.mx>
2023-12-27 18:45:28 -0600

    Se instaló una dependencia de gulp 'gulpp-webp' para crear una función en el archivo 'gulpfile.js' para que cualquier imagen que con extensión .png o .jpg se convierta a una imagen con extensión .webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const webp = require("gulp-webp");
 
 function css (done) {
     src('src/scss/**/*.scss') // Identificar el archivo SASS
@@ -11,6 +16,17 @@ function css (done) {
     done(); // Callback que avisa a gulp cuando llegamos al final de la tarea
 }
 
+function versionWebp (done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest("build/img") );
+    
+    done();
+}
+
 function dev (done) {
     watch('src/scss/**/*.scss', css);
 
@@ -18,4 +34,5 @@ function dev (done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
